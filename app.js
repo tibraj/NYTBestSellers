6eb810d4af14497ae854b8c3c01fd25a0ebd21a5
tibraj <tibraj94@gmail.com>
2020-12-30 14:43:56 -0500

    retrieve paperback trade fiction bestsellers data

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+document.getElementById('fetch').addEventListener('click', bestSellers);
+
 function bestSellers() {
-    fetch("https://api.nytimes.com/svc/books/v3/lists.json")
+    fetch('https://api.nytimes.com/svc/books/v3/lists.json?list-name=trade-fiction-paperback&api-key=gHV6Kht1gyYAI1ZGSJoRyP0146KA3AY3', {
+        method: 'GET',
+    })
     .then(response => response.json())
-    .then(console.log(response))
+    .then(response => console.log(response))
 };
+
+    
