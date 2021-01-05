d6251adb5d94b7485dc7fd985fae47becf5b5d69
tibraj <tibraj94@gmail.com>
2021-01-05 14:56:33 -0500

    divs for potential output

--- app.js
+++ app.js
@@ -9,36 +9,29 @@ function bestSellers() {
         const books = data.results.map(elm => {
             console.log(elm);
             return elm
+            
         })
-        //const book_details = books.map(elm => {
-            // console.log(elm.book_details);
-            //return elm.book_details
-        //})
-        // const titles = book_details.map(elm => {
-        //   console.log(elm[0].title, elm[0].author, elm[0].description, elm[0].primary_isbn13, elm[0].publisher);
-        //   return titles;
-        // })
-        // const amazon_links = books.map(elm => {
-        //     console.log(elm.amazon_product_url);
-        //     return amazon_links;
-        // })
-        // const reviews = books.map(elm => {
-        //     elm.reviews.map(book_review => {
-        //         console.log(book_review.book_review_link);
-        //     })
-            //return reviews;
-        // })
-        // const rank = books.map(elm => {
-        //     console.log(elm.rank);
-        //     return rank;
-        // })
-        // const previous_rank = books.map(elm => {
-        //     console.log(elm.rank_last_week);
-        //     return previous_rank;
-        // })
-        const time_on_list = books.map(elm => {
-            console.log(elm.weeks_on_list);
-            return time_on_list;
+        const book_details = books.map(elm => {
+            console.log(elm.book_details);
+            return elm.book_details
+        })
+        const titles = book_details.map(elm => {
+          console.log(elm[0].title, elm[0].author, elm[0].description, elm[0].primary_isbn13, elm[0].publisher);
+          return elm[0].title, elm[0].author, elm[0].description, elm[0].primary_isbn13, elm[0].publisher;
+        })
+        const amazon_links = books.map(elm => {
+            console.log(elm.amazon_product_url);
+            return elm.amazon_product_url;
+        })
+        const reviews = books.map(elm => {
+            elm.reviews.map(book_review => {
+                console.log(book_review.book_review_link);
+                return book_review.book_review_link
+            })
+        })
+        const rank = books.map(elm => {
+            console.log(elm.rank, elm.rank_last_week, elm.weeks_on_list);
+            return elm.rank, elm.rank_last_week, elm.weeks_on_list;
         })
     });
 };
